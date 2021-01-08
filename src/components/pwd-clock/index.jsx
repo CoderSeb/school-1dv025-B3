526e917f9ff8ce8fc9e340cf988627b1d798ecde
Sebastian <sa224ny@student.lnu.se>
2021-01-08 18:10:28 +0100

    PwdClock added JSdoc

--- src/components/pwd-clock/index.jsx
+++ src/components/pwd-clock/index.jsx
@@ -1,9 +1,25 @@
+/**
+ * Script file for the PwdClock component.
+ *
+ * @version 1.0.0
+ * @author Sebastian Åkerblom <sa224ny@student.lnu.se>
+ */
+
+/* eslint-disable no-unused-vars */
+
+// Imports
 import React, { useState, useEffect } from 'react'
 import './styles.css'
 
+/**
+ * PwdClock function that shows the local time.
+ *
+ * @returns {*} as the PwdClock component.
+ */
 const PwdClock = () => {
   const [time, setTime] = useState(new Date().toLocaleTimeString())
 
+  // Gets called when time is updated. Also clears the interval when component is removed.
   useEffect(() => {
     const intervalID = setInterval(() => {
       setTime(new Date().toLocaleTimeString())
